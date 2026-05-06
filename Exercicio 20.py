4026b5deb98daeef32eb81fccf99163145869968
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-05 19:29:55 -0900

    Exercicio 20 de lógica

--- Exercicio 20.py
+++ Exercicio 20.py
@@ -6,7 +6,7 @@ import time
 pygame.init()
 pygame.mixer.init()
 
-pygame.mixer.music.load("ex20.mp3")
+pygame.mixer.music.load("exercicio20.mp3")
 pygame.mixer.music.play()
 
 #Maneter o programa rodando enquanto a musica toca
