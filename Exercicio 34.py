fc5a71121e30b4d756b82ca83c5e312a58e31402
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-06 20:20:31 -0900

    Exercicio 34 de lógica

---
+++ Exercicio 34.py
@@ -0,0 +1,11 @@
+#Exercicio 34-
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+r1 = float(input("Primeiro segmento: "))
+r2 = float(input("Segundo segmento: "))
+r3 = float(input("Terceiro segmento: "))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 +r2:
+    print("Os segmentos digitados PODEM forma um Triângulo! ")
+else:
+    print("Os segmentos digitados acima NÃO podem formar um triângulo! ")
