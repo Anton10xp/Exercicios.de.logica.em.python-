1d8b642d961a66293858f610628eca11d817ae53
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-10 16:14:22 -0900

    Exercicio 45 de lógica

--- Exercicio 45.py
+++ Exercicio 45.py
@@ -1,5 +1,5 @@
 #Exercicio 45-
-# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+# Faça um programa que mostre na tela uma contagem regressiva de 10 até 0
 
 from time import sleep
 
