1a556b41d384b5dd2911403d5af97c0d8bd98a6d
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-10 23:06:32 -0900

    Exercicio 45 de lógica

--- Exercicio 45.py
+++ Exercicio 45.py
@@ -1,9 +1,8 @@
 #Exercicio 45-
-# Faça um programa que mostre na tela uma contagem regressiva de 10 até -
+# Faça um programa que mostre na tela uma contagem regressiva de 10 até 0
 
 from time import sleep
 
 for contador in range(10, -1, -1):
     print(contador)
-    
     sleep(0.5)
