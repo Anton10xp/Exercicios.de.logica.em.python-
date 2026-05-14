880e4a193138cf2ea4dc3e25a57a6faefea7c519
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-14 12:41:11 -0900

    Exercicio 47 de lógia

---
+++ Exercicio 47.py
@@ -0,0 +1,10 @@
+#Exercicio 47-
+#Faça um programa que calcule a soma entre todos os numeros impares que são multiples de tres e que se encontram em um intervalo de 1 até 500.
+
+soma = 0
+
+for i in range(1,501,2):
+    if i % 3 == 0:
+        soma = soma + i
+    
+    print(soma, end= " ")
