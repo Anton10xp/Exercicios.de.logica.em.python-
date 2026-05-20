24550ab66e6ce5ec1d3f77bdaed8e0efe09cd275
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-20 02:28:04 -0900

    Exercicio 50 de lógica

---
+++ Exercicio 50.py
@@ -0,0 +1,10 @@
+primeiro = int(input('Digite o primeiro termo: '))
+
+razao = int(input("Digite a razão: "))
+
+decimo = primeiro + (10 - 1) * razao
+
+for i in range(primeiro, decimo + razao, razao):
+    print(" {} ".format(i), end = " -- ")
+
+print("Fim")
