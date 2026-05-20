97a501da544687d0bf32928a21a9b2a29715eab0
unknown <antoniovieirafreitas2007@gmail.com>
2026-05-20 02:07:44 -0900

    Exercicio 49 de lógica

--- Exercicio 49.py
+++ Exercicio 49.py
@@ -1,13 +1,15 @@
 #Exercicio 49-
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
+cont = 0
 soma = 0
-contador = 0
+for i in range(6):
+    cont = cont + 1
+    numero = int(input("Digite o {}° número inteiro: ".format(cont)))
 
-for i in range(1,7):
-    numero = int(input("Digite o {}° número: ".format(i)))
     if numero % 2 == 0:
         soma = soma + numero
-        contador = contador + 1
+    
+print("A soma dos números impares é: {}".format(soma))
 
-print("A soma entre os {} numeros pares que você digiotu é igual a {}: ".format(contador, soma))
+  
